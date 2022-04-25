d49388415af3797a6936e36a72e3b3388c5a07be
parkyejin <uiop5809@naver.com>
2022-04-25 22:15:16 +0900

    가장 상위 컴포넌트에 프로바이더하여 전역 상태 관리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
+import AppStateProvider from "./providers/AppStateProviders";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    // 가장 상위 컴포넌트에 프로바이더하여 전역 상태 관리
+    <AppStateProvider>
+      <div className="App">
+        <Header />
+      </div>
+    </AppStateProvider>
   );
 }
 
