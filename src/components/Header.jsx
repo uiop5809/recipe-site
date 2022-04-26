e981567e33b8d646a7d6ed46eb39e5d785cc208d
parkyejin <uiop5809@naver.com>
2022-04-26 17:07:49 +0900

    스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import styled from "styled-components";
+import NavLinks from "./NavLinks";
+
 export default function Header() {
+  const Head = styled.header`
+    background: #f5f6f0;
+    text-align: center;
+    h1 {
+      font-size: 50px;
+      font-weight: 800;
+      padding: 30px 0;
+    }
+  `;
+
   return (
-    <header>
-      <h1> recipe site </h1>
+    <Head>
+      <h1> Recipe site </h1>
       <div> 검색 부분 </div>
-    </header>
+      <NavLinks />
+    </Head>
   );
 }
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,48 +1,91 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export default function NavLinks() {
-	return (
-		<ul>
-			<li>
-				<NavLink
-					to="/"
-					style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-				>
-					Home
-				</NavLink>
-			</li>
-			<li>
-				<NavLink
-					to="/korean"
-					style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-				>
-					한식
-				</NavLink>
-			</li>
-			<li>
-				<NavLink
-					to="/chinese"
-					style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-				>
-					중식
-				</NavLink>
-			</li>
-			<li>
-				<NavLink
-					to="/japanese"
-					style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-				>
-					일식
-				</NavLink>
-			</li>
-			<li>
-				<NavLink
-					to="/western"
-					style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
-				>
-					양식
-				</NavLink>
-			</li>
-		</ul>
-	);
+  const List = styled.div`
+    ul {
+      display: flex;
+      padding-top: 10px;
+      li {
+        box-sizing: border-box;
+        position: relative;
+        width: 20%;
+        height: 70px;
+        a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 20px 20px 0 0;
+          width: 100%;
+          height: 100%;
+          text-decoration: none;
+          color: #fff;
+          font-size: 18px;
+          font-weight: 800;
+        }
+      }
+    }
+  `;
+
+  return (
+    <List>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => ({
+              background: isActive ? "black" : "inherit",
+              color: isActive ? "white" : "black",
+            })}
+          >
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/korean"
+            style={({ isActive }) => ({
+              background: isActive ? "black" : "inherit",
+              color: isActive ? "white" : "black",
+            })}
+          >
+            한식
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/chinese"
+            style={({ isActive }) => ({
+              background: isActive ? "black" : "inherit",
+              color: isActive ? "white" : "black",
+            })}
+          >
+            중식
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/japanese"
+            style={({ isActive }) => ({
+              background: isActive ? "black" : "inherit",
+              color: isActive ? "white" : "black",
+            })}
+          >
+            일식
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/western"
+            style={({ isActive }) => ({
+              background: isActive ? "black" : "inherit",
+              color: isActive ? "white" : "black",
+            })}
+          >
+            양식
+          </NavLink>
+        </li>
+      </ul>
+    </List>
+  );
 }
