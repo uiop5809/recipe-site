0cabf8b92286c41c9833e18420b5cedc2a53c1fe
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-04-28 12:10:53 +0900

    hover 스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,30 @@
-import styled from "styled-components";
-import NavLinks from "./NavLinks";
+import styled from 'styled-components';
+import NavLinks from './NavLinks';
 
 const Head = styled.header`
-  background: #fae3d9;
-  text-align: center;
-  h1 {
-    font-size: 50px;
-    font-weight: 800;
-    padding: 40px 0;
-    color: #524a4e;
-  }
+	background: #fae3d9;
+	text-align: center;
+	h1 {
+		font-size: 50px;
+		font-weight: 800;
+		padding: 40px 0;
+		color: #632626;
+	}
 `;
 
 const Container = styled.div`
-  width: 70%;
-  margin: 0 auto;
+	width: 70%;
+	margin: 0 auto;
 `;
 
 export default function Header() {
-  return (
-    <Head>
-      <Container>
-        <h1> Recipe site </h1>
-        <div> 검색 부분 </div>
-        <NavLinks />
-      </Container>
-    </Head>
-  );
+	return (
+		<Head>
+			<Container>
+				<h1> Recipe site </h1>
+				<div> 검색 부분 </div>
+				<NavLinks />
+			</Container>
+		</Head>
+	);
 }
