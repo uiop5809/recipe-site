cfc2fc93e56f76be633cc46816711331d241e938
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-04-28 16:58:31 +0900

    px -> vh로 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,25 @@
-import styled from "styled-components";
-import Container from "./styled-component/Container";
+import styled from 'styled-components';
+import Container from './styled-component/Container';
 
 const Foot = styled.footer`
-  background: #2c2a29;
-  padding: 20px 0;
-  p {
-    color: #fff;
-    margin: 10px 0;
-  }
+	background: #2c2a29;
+	box-sizing: border-box;
+	padding: 1.8vh 0;
+	height: 10vh;
+	p {
+		font-size: 1.36vh;
+		color: #fff;
+		margin: calc((10vh - 6.32vh) / 3) 0;
+	}
 `;
 
 export default function Footer() {
-  return (
-    <Foot>
-      <Container>
-        <p>만든이: 박예진, 정태우</p>
-        <p>© 2022 SSI, All rights reserved.</p>
-      </Container>
-    </Foot>
-  );
+	return (
+		<Foot>
+			<Container>
+				<p>만든이: 박예진, 정태우</p>
+				<p>© 2022 SSI, All rights reserved.</p>
+			</Container>
+		</Foot>
+	);
 }
