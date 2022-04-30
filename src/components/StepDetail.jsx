dfc70d86bcb64f44f0e05ee6c8a838cc14d4056d
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2022-04-30 22:14:42 +0900

    타이머 수정

--- src/components/StepDetail.jsx
+++ src/components/StepDetail.jsx
@@ -1,110 +1,114 @@
-import StyledButton from "./styled-component/StyledButton";
-import { useNavigate } from "react-router-dom";
-import { RiArrowGoBackFill } from "react-icons/ri";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import styled from "styled-components";
-import Timer from "./Timer";
+import StyledButton from './styled-component/StyledButton';
+import { useNavigate } from 'react-router-dom';
+import { RiArrowGoBackFill } from 'react-icons/ri';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import styled from 'styled-components';
+import Timer from './Timer';
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const StyledSwiper = styled(Swiper)`
-  width: 1300px;
-  height: 70vh;
-  box-sizing: border-box;
-  position: relative;
-  padding: 20px 0;
+	width: 1300px;
+	height: 70vh;
+	box-sizing: border-box;
+	position: relative;
+	padding: 20px 0;
 
-  .swiper-slide {
-    height: 400px;
-    .box {
-      display: flex;
-      flex-wrap: wrap;
-      height: 400px;
-      padding: 0 20px;
+	.swiper-slide {
+		height: 400px;
+		.box {
+			display: flex;
+			flex-wrap: wrap;
+			height: 400px;
+			padding: 0 20px;
 
-      .content {
-        margin: auto;
-        height: 200px;
-        width: 300px;
-        line-height: 1.4em;
-        font-size: 18px;
-        li {
-          margin: auto;
-        }
-      }
-      .timer {
-        background: grey;
-        border-radius: 10px;
-        color: white;
-        width: 110px;
-        height: 50px;
-        margin: auto;
-        text-align: center;
-        line-height: 50px;
-        font-size: 20px;
-      }
-    }
-    border: 5px solid #fff5ee;
-  }
+			.content {
+				margin: auto;
+				height: 200px;
+				width: 300px;
+				line-height: 1.4em;
+				font-size: 18px;
+				li {
+					margin: auto;
+				}
+			}
+			.timer {
+				background: grey;
+				border-radius: 10px;
+				color: white;
+				width: 110px;
+				height: 50px;
+				margin: auto;
+				text-align: center;
+				line-height: 50px;
+				font-size: 20px;
+				font-weight: 800;
+			}
+		}
+		border: 5px solid #fff5ee;
+	}
 `;
 
 export default function StepDetail({ id, steps }) {
-  const navigate = useNavigate();
-  // const minutes = steps.map((step) =>
-  //   String(parseInt(step.time / 60)).padStart(2, "0")
-  // );
-  // const seconds = steps.map((step) => String(step.time % 60).padStart(2, "0"));
+	const navigate = useNavigate();
+	// const minutes = steps.map((step) =>
+	//   String(parseInt(step.time / 60)).padStart(2, "0")
+	// );
+	// const seconds = steps.map((step) => String(step.time % 60).padStart(2, "0"));
 
-  return (
-    <>
-      <StyledSwiper
-        spaceBetween={40}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        centeredSlides={true}
-      >
-        <ol style={{ listStyleType: "decimal" }}>
-          {steps.map((step) => (
-            <SwiperSlide>
-              {({ isActive }) => (
-                <div className="box">
-                  {isActive ? (
-                    <>
-                      <div className="content">
-                        <li key={step.content}>{step.content}</li>
-                      </div>
-                      <div className="timer">
-                        <Timer t={step.time} />
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div className="content">
-                        <li key={step.content}>{step.content}</li>
-                      </div>
-                      <div className="timer">
-                        {parseInt(step.time / 60)} : {step.time % 60}
-                      </div>
-                    </>
-                  )}
-                </div>
-              )}
-            </SwiperSlide>
-          ))}
-        </ol>
-      </StyledSwiper>
-      <StyledButton
-        onClick={() => {
-          navigate(-1); // 뒤로가기
-        }}
-      >
-        <RiArrowGoBackFill className="goBack" />
-      </StyledButton>
-    </>
-  );
+	return (
+		<>
+			<StyledSwiper
+				spaceBetween={40}
+				slidesPerView={3}
+				navigation
+				pagination={{ clickable: true }}
+				centeredSlides={true}
+			>
+				<ol style={{ listStyleType: 'decimal' }}>
+					{steps.map((step) => (
+						<SwiperSlide>
+							{({ isActive }) => (
+								<div className="box">
+									{isActive ? (
+										<>
+											<div className="content">
+												<li key={step.content}>{step.content}</li>
+											</div>
+											<div className="timer">
+												<Timer t={step.time} />
+											</div>
+										</>
+									) : (
+										<>
+											<div className="content">
+												<li key={step.content}>{step.content}</li>
+											</div>
+											<div className="timer">
+												{parseInt(step.time / 60) < 10 ? '0' : ''}
+												{parseInt(step.time / 60)} :
+												{parseInt(step.time % 60) < 10 ? ' 0' : ' '}
+												{step.time % 60}
+											</div>
+										</>
+									)}
+								</div>
+							)}
+						</SwiperSlide>
+					))}
+				</ol>
+			</StyledSwiper>
+			<StyledButton
+				onClick={() => {
+					navigate(-1); // 뒤로가기
+				}}
+			>
+				<RiArrowGoBackFill className="goBack" />
+			</StyledButton>
+		</>
+	);
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,34 +1,36 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
 
 const Timer = ({ t }) => {
-  const time = useRef(t);
-  const timerld = useRef(null);
-  const [min, setMin] = useState(parseInt(time.current / 60));
-  const [sec, setSec] = useState(time.current % 60);
+	const time = useRef(t);
+	const timerld = useRef(null);
+	const [min, setMin] = useState(parseInt(time.current / 60));
+	const [sec, setSec] = useState(time.current % 60);
 
-  useEffect(() => {
-    timerld.current = setInterval(() => {
-      setMin(parseInt(time.current / 60));
-      setSec(time.current % 60);
-      time.current -= 1;
-    }, 1000);
+	useEffect(() => {
+		timerld.current = setInterval(() => {
+			setMin(parseInt(time.current / 60));
+			setSec(time.current % 60);
+			time.current -= 1;
+		}, 1000);
 
-    return () => clearInterval(timerld.current);
-  }, []); // 최초에만 실행
+		return () => clearInterval(timerld.current);
+	}, []); // 최초에만 실행
 
-  useEffect(() => {
-    // 타임 아웃이 발생했을 경우
-    if (time.current <= 0) {
-      console.log("타임 아웃");
-      clearInterval(timerld.current);
-    }
-  }, [sec]);
+	useEffect(() => {
+		// 타임 아웃이 발생했을 경우
+		if (time.current <= 0) {
+			console.log('타임 아웃');
+			clearInterval(timerld.current);
+		}
+	}, [sec]);
 
-  return (
-    <div>
-      {min} : {sec}
-    </div>
-  );
+	return (
+		<div>
+			{min < 10 ? '0' : ''}
+			{min} :{sec < 10 ? ' 0' : ' '}
+			{sec}
+		</div>
+	);
 };
 
 export default Timer;
